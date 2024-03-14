5f2e1a47b6086f09e87a6126c41160da44472aaf
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-03-14 20:16:02 -0300

    Exercício 1 e 2

---
+++ src/main/kotlin/lambdas/Exerc2.kt
@@ -0,0 +1,18 @@
+package lambdas
+
+fun multiplica(x: Int, y: Int): Int{
+    return x * y
+}
+fun main(args: Array<String>){
+
+    println("Digite o primeiro número: ")
+    val X = readLine()!!.toInt()
+
+    print("Digite o segundo número: ")
+    val Y = readLine()!!.toInt()
+
+    val resultado = multiplica(X, Y)
+
+    println("O resultado da multiplicação é = $resultado")
+
+}
